fb0b425f8dfc54a2b35088a36dcf920e73525bed
Niksa7 <nikitasoroka228@gmail.com>
2023-11-23 23:28:45 +0300

    Выявление движения на видео.

--- Lab 5/motion_detection.py
+++ Lab 5/motion_detection.py
@@ -1,7 +1,7 @@
-import cv2
 import cv2 as cv
 
-def motion_detection(src, output, ksize, sigma, porog, base_area):
+
+def motion_detection(src, output, ksize, sigma, threshold, base_area):
     # Читаем видео из файла
     cap = cv.VideoCapture(src, cv.CAP_ANY)
 
@@ -16,12 +16,15 @@ def motion_detection(src, output, ksize, sigma, porog, base_area):
     # Гауссовское размытие
     gg_image = cv.GaussianBlur(gray_image, (ksize, ksize), sigma)
     # Кодек
-    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
     # Высота и ширина кадра для записи
     width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-    video_writer = cv.VideoWriter('Output_videos/' + output + '.mp4', fourcc, 24, (width, height))
+    fps = cap.get(cv.CAP_PROP_FPS)
+
+    video_writer = cv.VideoWriter('Output_videos/' + output + '.mov', fourcc, fps, (width, height))
+    video_writer_contours = cv.VideoWriter('Output_videos/' + output + '_countrs.mov', fourcc, fps, (width, height))
 
     while True:
 
@@ -30,8 +33,7 @@ def motion_detection(src, output, ksize, sigma, porog, base_area):
 
         # Новый кадр
         ret, next_frame = cap.read()
-        
-        # Eсли чтение неуспешно, остановить цикл
+
         if not ret:
             break
 
@@ -42,7 +44,7 @@ def motion_detection(src, output, ksize, sigma, porog, base_area):
         frame_diff = cv.absdiff(gg_image, prev_image)
 
         # Применяет пороговое(бинарное) значение к изображению. Тип пороговой обработки
-        porog, frame_treshold = cv.threshold(frame_diff, porog, 255, cv.THRESH_BINARY)
+        _, frame_treshold = cv.threshold(frame_diff, threshold, 255, cv.THRESH_BINARY)
 
         # Поиск контуров. Режим поиска контура. Метод аппроксимации контура.
         contours, hierarchy = cv.findContours(frame_treshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -53,27 +55,42 @@ def motion_detection(src, output, ksize, sigma, porog, base_area):
 
             if area > base_area:
                 video_writer.write(next_frame)
+                countrs_frame = next_frame.copy()
+                cv.drawContours(countrs_frame, [contour], 0, (0, 255, 0), 2)
+                video_writer_contours.write(countrs_frame)
 
     video_writer.release()
+    video_writer_contours.release()
+
 
 # Размытие
-ksize = 3
-sigma = 50
+ksize = 7
+sigma = 2
 # Обнаружение ложных срабатываний
-porog = 60
-base_area = 20
-motion_detection('Videos/LR4_main_video.mov', 'output_video_1', ksize, sigma, porog, base_area)
+threshold = 120
+base_area = 50
+motion_detection('Videos/LR4_main_video.mov', 'final_video_1', ksize, sigma, threshold, base_area)
 
+# Размытие
+ksize = 5
+sigma = 0
+# Обнаружение ложных срабатываний
+threshold = 30
+base_area = 10
+motion_detection('Videos/LR4_main_video.mov', 'final_video_2', ksize, sigma, threshold, base_area)
 
+# Размытие
+ksize = 9
+sigma = 2
+# Обнаружение ложных срабатываний
+threshold = 35
+base_area = 8
+motion_detection('Videos/LR4_main_video.mov', 'final_video_3', ksize, sigma, threshold, base_area)
 
-
-
-
-
-
-
-
-
-
-
-
+# Размытие
+ksize = 7
+sigma = 1.5
+# Обнаружение ложных срабатываний
+threshold = 45
+base_area = 6
+motion_detection('Videos/LR4_main_video.mov', 'final_video_4', ksize, sigma, threshold, base_area)
